d041a979d98b4c3c815312c904752f3f4b8e4ca2
yshungria-uniandes <ys.hungria@uniandes.edu.co>
2024-04-02 23:24:45 -0500

    Usuarios CRUD terminado

--- Python/FLASK-MySQL/usuarios_crud/user.py
+++ Python/FLASK-MySQL/usuarios_crud/user.py
@@ -19,6 +19,22 @@ class User:
         return users
     
     @classmethod
+    def get_one(cls, data):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        result = connectToMySQL('esquema_usuarios').query_db(query, data)
+        return cls(result[0])
+    
+    @classmethod
+    def update(cls, data):
+        query = "UPDATE users SET first_name = %(fname)s, last_name = %(lname)s, email = %(email)s, updated_at = NOW() WHERE id = %(id)s;"
+        return connectToMySQL('esquema_usuarios').query_db( query, data )
+    
+    @classmethod
+    def delete(cls, data):
+        query = "DELETE FROM users WHERE id = %(id)s;"
+        return connectToMySQL('esquema_usuarios').query_db( query, data )
+    
+    @classmethod
     def save(cls, data ):
         query = "INSERT INTO users ( first_name , last_name , email , created_at, updated_at ) VALUES ( %(fname)s , %(lname)s , %(email)s , NOW() , NOW() );"
         return connectToMySQL('esquema_usuarios').query_db( query, data )
