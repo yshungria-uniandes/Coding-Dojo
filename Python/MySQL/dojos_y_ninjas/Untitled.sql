a0797aa6de8387d4999fafa325e9986c90d29157
yshungria-uniandes <ys.hungria@uniandes.edu.co>
2024-04-07 18:23:40 -0500

    Ajustes CRUD Modularizado Dojos y Ninjas

--- Python/MySQL/dojos_y_ninjas/Untitled.sql
+++ Python/MySQL/dojos_y_ninjas/Untitled.sql
@@ -1,26 +1,9 @@
 -- Desactivar el modo seguro de actualizaciones SQL
 SET SQL_SAFE_UPDATES = 0;
 
-USE esquema_amistades;
+USE login_reg;
 
--- Consulta: crea 6 usuarios nuevos
--- INSERT INTO users (first_name, last_name) VALUES ('Amy', 'Giver'), ('Eli', 'Byers'), ('Big', 'Bird'), ('La rana', 'René'), ('Marky', 'Mark'), ('La rana', 'René');
+-- INSERT INTO friends(first_name, last_name, occupation)
+-- VALUES ('John', 'Doe', 'Engineer'), ('Jane', 'Smith', 'Doctor'), ('Alice', 'Johnson', 'Teacher');
 
--- Consulta: haz que el usuario 1 sea amigo del usuario 2, 4 y 6
--- INSERT INTO friendships (user_id, friend_id) VALUES (1, 2), (1, 4), (1, 6);
-
--- Consulta: haz que el usuario 2 sea amigo del usuario 1, 3 y 5
--- INSERT INTO friendships (user_id, friend_id) VALUES (2, 1), (2, 3), (2, 5);
-
-
--- Consulta: haz que el usuario 3 sea amigo del usuario 2 y 5
--- INSERT INTO friendships (user_id, friend_id) VALUES (3, 2), (3, 5);
-
--- Consulta: haz que el usuario 4 sea amigo del usuario 3
--- INSERT INTO friendships (user_id, friend_id) VALUES (4, 3);
-
--- Consulta: haz que el usuario 5 sea amigo del usuario 1 y 6
--- INSERT INTO friendships (user_id, friend_id) VALUES (5, 1), (5, 6);
-
--- Consulta: haz que el usuario 6 sea amigo del usuario 2 y 3
-INSERT INTO friendships (user_id, friend_id) VALUES (6, 2), (6, 3);
+SELECT * FROM burgers
