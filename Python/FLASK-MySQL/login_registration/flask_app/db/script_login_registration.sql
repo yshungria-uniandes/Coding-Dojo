fb26b7b91954d9fe98cc89901c0fe7db731cf278
yshungria-uniandes <ys.hungria@uniandes.edu.co>
2024-04-08 01:33:42 -0500

    Inicio de sesión y Registro terminado

---
+++ Python/FLASK-MySQL/login_registration/flask_app/db/script_login_registration.sql
@@ -0,0 +1,38 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema login_register_schema
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `login_register_schema` ;
+
+-- -----------------------------------------------------
+-- Schema login_register_schema
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `login_register_schema` ;
+USE `login_register_schema` ;
+
+-- -----------------------------------------------------
+-- Table `login_register_schema`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `login_register_schema`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NULL,
+  `last_name` VARCHAR(45) NULL,
+  `email` VARCHAR(255) NULL,
+  `password` VARCHAR(255) NULL,
+  `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
