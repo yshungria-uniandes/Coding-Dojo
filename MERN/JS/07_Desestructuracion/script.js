eb768165c43c4c2cea8d63b28f443be42f23b378
yshungria-uniandes <ys.hungria@uniandes.edu.co>
2024-07-17 14:54:41 -0500

    Desestructuración JS getter y setter (useState - Hook)

--- MERN/JS/07_Desestructuracion/script.js
+++ MERN/JS/07_Desestructuracion/script.js
@@ -15,28 +15,56 @@
 
 // console.log(codigoEncriptado) // Salida: 12345
 
-const alterego = {
-  nombre: "Diana",
-  apellido: "Prince",
-  email: "diana.prince@themyscira.com",
-  contraseña: "Am@z0nW@rri0r",
-  usuario: "wonderwoman",
-  direcciones: [
-    {
-      direccion: "Themyscira Palace",
-      ciudad: "Themyscira",
-      codigoPostal: "0001",
-    },
-    {
-      direccion: "7000 Hollywood Blvd",
-      ciudad: "Los Angeles",
-      codigoPostal: "90028",
-    },
-  ],
-  creadoEn: 1714866113353,
-};
-const { direcciones } = alterego;
-const [primeraDireccion, segundaDireccion] = direcciones;
+// const alterego = {
+//   nombre: "Diana",
+//   apellido: "Prince",
+//   email: "diana.prince@themyscira.com",
+//   contraseña: "Am@z0nW@rri0r",
+//   usuario: "wonderwoman",
+//   direcciones: [
+//     {
+//       direccion: "Themyscira Palace",
+//       ciudad: "Themyscira",
+//       codigoPostal: "0001",
+//     },
+//     {
+//       direccion: "7000 Hollywood Blvd",
+//       ciudad: "Los Angeles",
+//       codigoPostal: "90028",
+//     },
+//   ],
+//   creadoEn: 1714866113353,
+// };
+// // const { direcciones } = alterego;
+// // const [primeraDireccion, segundaDireccion] = direcciones;
 
-console.log(primeraDireccion);
-console.log(segundaDireccion);
+// // console.log(primeraDireccion);
+// // console.log(segundaDireccion);
+
+// const { direcciones } = alterego;
+// const [, segundaDireccion] = direcciones;
+
+// console.log("Segunda Dirección:", segundaDireccion);
+
+let cantidad = 10;
+
+const manejarCantidad = [
+  // Índice 0; Una Función Getter
+  function () {
+    console.log("Cantidad actual: " + cantidad);
+    return cantidad;
+  },
+  // Índice 1; Una Función Setter
+  function (nuevaCantidad) {
+    console.log("Cantidad actualizada: " + nuevaCantidad);
+    cantidad = nuevaCantidad;
+    return cantidad;
+  },
+];
+
+// Acá desestructuramos getter (getCantidad) y setter (setCantidad)
+const [getCantidad, setCantidad] = manejarCantidad;
+
+getCantidad(); // Salida: Cantidad actual: 10
+setCantidad(20); // Salida: Cantidad actualizada: 20
+getCantidad(); // Salida: Cantidad actual: 20
